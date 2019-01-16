b90f83c5c69daa3340b815e3e4fb2397065f7886
mahmoudabdelhares <46143229+mahmoudabdelhares@users.noreply.github.com>
2019-01-16 22:50:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,16 +4,4 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-token= NTMzMTU4NjEyNDAzNDg2NzIx.DyEkkA.pGWLL00ogt2jQKu59DfzhcPATHQ
-owner= 490320866391883788
-prefix= A
-game= Ahelp
-stayinchannel=true
-songinstatus=true
 client.login(process.env.BOT_TOKEN);
